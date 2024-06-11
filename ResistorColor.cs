f8dd0aa39821b3ed7cae9b75569e0c927a0f3e6c
Dmitriy Sukharev <dmitriy.pastanovskiy@gmail.com>
2024-06-11 12:23:30 +0300

    initial commit

---
+++ ResistorColor.cs
@@ -0,0 +1,12 @@
+using System;
+
+public static class ResistorColor
+{
+    private static readonly string[] ResistorColors = 
+        ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" ];
+    public static int ColorCode(string color) =>
+        Array.IndexOf(ResistorColors, color);
+
+    public static string[] Colors() => ResistorColors;
+
+}
